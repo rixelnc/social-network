0aaa8ccf5eca24c6de269655c2fbafdccf3a1880
natasha <rixeltd@gmail.com>
2022-02-15 17:36:18 +0400

    24

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,9 +3,15 @@ import s from './MyPosts.module.css'
 import Post from "./Post/Post";
 
 const MyPosts = (props) => {
+
+    let postData = [
+        {id: 1, message: 'Hi, how are you?', likeCount: 12},
+        {id: 2, message: 'It\'s my first post', likeCount: 11},
+    ]
+
     return (
         <div className={s.postsBlock}>
-           <h3> My posts</h3>
+            <h3> My posts</h3>
             <div>
                 <div>
                     <textarea></textarea>
@@ -15,8 +21,8 @@ const MyPosts = (props) => {
                 </div>
             </div>
             <div className={s.posts}>
-                <Post message='Hi, how are you?' likeCount=' 0'/>
-                <Post message="It's my first post" likeCount=' 23'/>
+                <Post message={postData[0].message} likeCount={postData[0].likeCount}/>
+                <Post message={postData[1].message} likeCount={postData[1].likeCount}/>
             </div>
         </div>
     )
