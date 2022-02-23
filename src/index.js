6b8c4d5ae8b3240402753e494f076b192e3d931d
natasha <rixeltd@gmail.com>
2022-02-23 17:56:08 +0400

    42

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {BrowserRouter} from "react-router-dom";
-import store from "./redux/state";
+import store from "./redux/redux-store";
 
 let rerenderEntireTree = (state) => {
     ReactDOM.render(
@@ -19,7 +19,10 @@ let rerenderEntireTree = (state) => {
 }
 rerenderEntireTree(store.getState());
 
-store.subscribe(rerenderEntireTree);
+store.subscribe(() => {
+    let state = store.getState()
+        rerenderEntireTree(state);
+});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -1,25 +1,35 @@
 const ADD_POST = 'ADD-POST';
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
 
-const profileReducer = (state, action) => {
-  switch (action.type){
-      case ADD_POST:
-          let newPost = {
-              id: 5,
-              message: state.newPostText,
-              likeCount: 0
-          };
-          state.posts.push(newPost);
-          state.newPostText = ' ';
-          return state;
+let initialState = {
+    posts: [
+        {id: 1, message: 'Hi, how are you?', likeCount: 12},
+        {id: 2, message: 'It\'s my first post', likeCount: 11},
+        {id: 3, message: 'Blabla', likeCount: 18},
+        {id: 4, message: 'Dada', likeCount: 1}
+    ],
+    newPostText: 'it-kamasutra.com'
+}
 
-      case UPDATE_NEW_POST_TEXT:
-          state.newPostText = action.newText;
-          return state;
+const profileReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case ADD_POST:
+            let newPost = {
+                id: 5,
+                message: state.newPostText,
+                likeCount: 0
+            };
+            state.posts.push(newPost);
+            state.newPostText = ' ';
+            return state;
 
-      default:
-          return state;
-  }
+        case UPDATE_NEW_POST_TEXT:
+            state.newPostText = action.newText;
+            return state;
+
+        default:
+            return state;
+    }
 }
 
 export const addPostActionCreator = () => ({type: ADD_POST});
