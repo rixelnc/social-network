9c5eab30ea67dfa12d777320dbfb09f7c01df07a
natasha <rixeltd@gmail.com>
2022-03-03 14:31:17 +0400

    63

---
+++ src/api/api.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+const instance = axios.create({
+    withCredentials: true,
+    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+    headers: {
+        'API-KEY': 'caa6f2a9-f46e-4530-9cba-321d991b41a2'
+    }
+})
+
+export const usersAPI = {
+    getUsers(currentPage = 1, pageSize = 10) {
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`)
+            .then(response => {
+                return response.data;
+            });
+    }
+}
+// export const getUsers2 = (currentPage = 1, pageSize = 10) => {
+//     return instance.get(`follow?page=${currentPage}&count=${pageSize}`)
+//         .then(response => {
+//             return response.data;
+//         });
+// }
+
