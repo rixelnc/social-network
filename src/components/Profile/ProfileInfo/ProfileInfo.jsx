59519ce978a72708373b1cac4ce0b27b3eb9c207
natasha <rixeltd@gmail.com>
2022-04-20 20:03:26 +0400

    97

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -20,11 +20,44 @@ const ProfileInfo = ({profile, status, updateStatus, isOwner, savePhoto}) => {
                 <div className={s.descriptionBlock}>
                     <img className={s.userPhoto} src={profile.photos.large || userPhoto}/>
                     {isOwner && <input type={'file'} onChange={onMainPhotoSelected}/>}
+                    <ProfileData profile={profile}/>
                     <ProfileStatusWithHooks status={status} updateStatus={updateStatus}/>
                 </div>
             </div>
         </div>
     )
+}
+
+const ProfileData = ({profile}) => {
+    return <div>
+        <div>
+            <b>Full name</b>: {profile.fullName}
+        </div>
+        <div>
+            <b>Looking for job</b>: {profile.lookingForAJob ? 'yes' : 'no'}
+        </div>
+        {profile.lookingForAJob &&
+        <div>
+            <b>My professional skills</b>: {profile.lookingForAJobDeckription}
+        </div>}
+        <div>
+            <b>About me</b>:{profile.aboutMe}
+        </div>
+        <div>
+            <b>Contacts</b>:{Object.keys(profile.contacts).map(key => {
+            return (
+                <Contact contactTitle={key} contactValue={profile.contacts[key]}/>
+            )
+        })}
+        </div>
+    </div>
+
+}
+
+const Contact = ({contactTitle, contactValue}) => {
+    return <div className={s.contact}>
+        <b>{contactTitle}</b>:{contactValue}
+    </div>
 }
 
 export default ProfileInfo;
